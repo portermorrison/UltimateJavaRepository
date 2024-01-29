94ab87ff21ad42fb45fd3a784058e78eef66ff2f
portermorrison <24portermorrison@usd266.com>
2024-01-29 10:29:41 -0600

    change

--- Unit03/Topic3_1/Main.java
+++ Unit03/Topic3_1/Main.java
@@ -2,7 +2,13 @@ package Unit03.Topic3_1;
 
 public class Main {
   public static void main(String[] args) {
+    int a = 10;
+    int b = 5 * 2;
+    System.out.println(a + "==" + b);
 
+    String name = "porter";
+
+    name.substring(0, 1);
   }
 
 }
