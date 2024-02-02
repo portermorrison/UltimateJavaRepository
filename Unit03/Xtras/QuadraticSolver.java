144730738d4db2d20eb23ce49b0b2e15a3f2c067
portermorrison <24portermorrison@usd266.com>
2024-02-02 10:25:24 -0600

    quadratic solver progress

--- Unit03/Xtras/QuadraticSolver.java
+++ Unit03/Xtras/QuadraticSolver.java
@@ -1,7 +1,24 @@
 package Unit03.Xtras;
+import java.util.Scanner;
 
 public class QuadraticSolver {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please input quadratic equation... first enter a");
+        int a  = scan.nextInt();
+        System.out.println("now enter b");
+        int b = scan.nextInt();
+        System.out.println("now enter c");
+        int c = scan.nextInt();
+
+        scan.close();
+
+        System.out.println("equation: a + b + c = 0");
+        if noSolutions;{
+            System.out.println("No Solutions");
+        } else 
+
 
     }
 }
