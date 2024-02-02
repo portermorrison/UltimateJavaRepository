7eecdeeccdb94f5e831c4ee12620656114d96885
portermorrison <24portermorrison@usd266.com>
2024-02-02 09:38:37 -0600

    solved spinnergame frq

--- Unit03/Topic3_5/SpinnerGame.java
+++ Unit03/Topic3_5/SpinnerGame.java
@@ -9,15 +9,30 @@ public class SpinnerGame {
 
     public int playRound() {
         int playerScore = spin(1,10);
-         
         int computerScore = spin(2,8);
 
         if (playerScore > computerScore) {
             int points = (int) Math.abs(playerScore - computerScore);
-            String win = "YOU WIN" + points;
+            String win = "YOU WIN!" + points + " points";
             System.out.println(win);
-        } else if ()
-
+        } else if (playerScore < computerScore) {
+            int points =  (playerScore - computerScore);
+            String lose = "YOU LOSE" + points;
+            System.out.println(lose);
+        } else {
+            playerScore = spin(1,10);
+            computerScore = spin(2,8);
+    
+            if (playerScore > computerScore) {
+                String win = "YOU WIN" + 1;
+                System.out.println(win);
+            } else if (playerScore < computerScore) {
+                String lose = "YOU LOSE" + -1;
+                System.out.println(lose);
+            } else {
+                System.out.println("tied game");
+            }
+        }
 
         return 0;
     }
