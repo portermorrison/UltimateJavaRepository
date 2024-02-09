b04e18e7c5d3bede64fc78a489b3fd2d51c5673c
Daniel Scislowski <mr.scislowski@gmail.com>
2024-02-08 18:19:01 -0600

    Add StringLoops assignment and tests

---
+++ Unit04/StringLoops.java
@@ -0,0 +1,56 @@
+package Unit04;
+
+public class StringLoops {
+
+    public static String doubleChar(String str) {
+        return "";
+    }
+
+    public static int countCode(String str) {
+        return 0;
+    }
+
+    public static boolean bobThere(String str) {
+        return false;
+    }
+
+    public static boolean prefixAgain(String str, int n) {
+        return false;
+    }
+
+    public static boolean sameStarChar(String str) {
+        return false;
+    }
+
+    public static String starOut(String str) {
+        return "";
+    }
+
+    public static int countHi(String str) {
+        return 0;
+    }
+
+    public static boolean endOther(String a, String b) {
+        return false;
+    }
+
+    public static boolean xyBalance(String str) {
+        return false;
+    }
+
+    public static String plusOut(String str, String word) {
+        return "";
+    }
+
+    public static boolean catDog(String str) {
+        return false;
+    }
+
+    public static String mixString(String a, String b) {
+        return "";
+    }
+
+    public static String repeatSeparator(String word, String sep, int count) {
+        return "";
+    }
+}
