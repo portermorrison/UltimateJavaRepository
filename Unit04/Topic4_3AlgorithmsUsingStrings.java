9883db8a77eb9bd6b10ac717579c0f8c84305520
portermorrison <24portermorrison@usd266.com>
2024-02-14 10:11:24 -0600

    change

--- Unit04/Topic4_3AlgorithmsUsingStrings.java
+++ Unit04/Topic4_3AlgorithmsUsingStrings.java
@@ -2,7 +2,11 @@ package Unit04;
 
 public class Topic4_3AlgorithmsUsingStrings {
     public static void main(String[] args) {
+        String s = "a black cat";
 
+        for (int i = 0; i < s.length(); i++) {
+         System.out.println(s.substring(i, i + 1));
+        }
     }
 }
 
