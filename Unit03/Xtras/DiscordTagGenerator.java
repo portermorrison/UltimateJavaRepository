099046d1b8a848dbe37ea0874d1f4ce0ece71c9c
portermorrison <24portermorrison@usd266.com>
2024-02-21 10:11:28 -0600

    unit 3 xstras done, progress on unit 4

--- Unit03/Xtras/DiscordTagGenerator.java
+++ Unit03/Xtras/DiscordTagGenerator.java
@@ -1,7 +1,26 @@
 package Unit03.Xtras;
 
+import java.util.Scanner;
+
 public class DiscordTagGenerator {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in); 
+
+        System.out.println("Please input username");
+        String username = scan.nextLine();
+
+        String tag = "";
+        tag += (int) (Math.random()* 9);
+        tag += (int) (Math.random()* 9);
+        tag += (int) (Math.random()* 9);
+        tag += (int) (Math.random()* 9);
+    
+        System.out.println(username + tag);
+    
+        
+
+
+
 
     }
 }
